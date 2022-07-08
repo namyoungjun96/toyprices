4b90e484646c84051170dba58efcfb137de0332f
namyoungjun96 <ehdgornltls@naver.com>
2022-07-08 21:30:24 +0900

    preferences

---
+++ src/main/java/toy/inflation/toyprices/ToypricesApplication.java
@@ -0,0 +1,13 @@
+package toy.inflation.toyprices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToypricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToypricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/inflation/toyprices/ToypricesApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.inflation.toyprices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToypricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
