6654fc01ec252b338661c9a82351b9342b229df8
jinchaeeun <kbg05032@naver.com>
2022-07-12 23:22:00 +0900

    RestTestController 들여쓰기 수정 및 서비스 키 별도 클래스 파일로 관리 이후 Auth_key는 로컬에서만 인증키 정보 저장 후 완료할 때까지 당분간 추적중지할것.

---
+++ src/main/java/toy/inflation/toyprices/controller/Auth_key.java
@@ -0,0 +1,5 @@
+package toy.inflation.toyprices.controller;
+
+public class Auth_key {
+	public static String key = ""; //부여받은 인코딩 인증키 정보 입력
+}
--- src/main/java/toy/inflation/toyprices/controller/RestTestController.java
+++ src/main/java/toy/inflation/toyprices/controller/RestTestController.java
@@ -9,33 +9,35 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import static toy.inflation.toyprices.controller.Auth_key.key; //serviceKey 인증정보
 
 @RestController
 public class RestTestController {
 
     @GetMapping("/api")
     public String callApi() throws IOException {
-        StringBuilder result = new StringBuilder();
-       
-        	String urlStr = "http://apis.data.go.kr/5080000/pricesInfoService/getPricesInfo?" + 
-					"serviceKey=" + //부여받은 serviceKey 정보 입력
-					"&pageNo=1" +
-					"&numOfRows=10";
-            URL url = new URL(urlStr);
-            
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            
-            BufferedReader br;
-            br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+    	StringBuilder result = new StringBuilder();
+        
+        
+    	String urlStr = "http://apis.data.go.kr/5080000/pricesInfoService/getPricesInfo?" + 
+				"serviceKey=" + key + //부여받은 serviceKey 정보 입력
+				"&pageNo=1" +
+				"&numOfRows=10";
+        URL url = new URL(urlStr);
+        
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        
+        BufferedReader br;
+        br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
 
-            String returnLine;
-            
-            while((returnLine = br.readLine()) != null) {
-                result.append(returnLine+"n\r");
-            }
-            
-            urlConnection.disconnect();
+        String returnLine;
+        
+        while((returnLine = br.readLine()) != null) {
+            result.append(returnLine+"n\r");
+        }
+        
+        urlConnection.disconnect();
             
         return result.toString();
     }
